01e670a928d667c0d3a0e8bb94f85866b222d901
youcef_kemiche <y.kemiche06@hotmail.com>
2022-05-25 17:35:45 +0200

    added numpy images despeckling

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ykemiche/deepdespeckling',
-    version='0.3.5',
+    version='0.3.8',
     zip_safe=False,
 )
