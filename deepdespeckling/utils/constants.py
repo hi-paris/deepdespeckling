d4e1f4570ddc321da523c429fcf62d7d70d6ebb4
Hadrien Mariaccia <hadrienmariaccia@gmail.com>
2023-11-09 11:21:26 +0100

    [HI-4] Remove useless constants

--- deepdespeckling/utils/constants.py
+++ deepdespeckling/utils/constants.py
@@ -1,10 +1,5 @@
 from scipy import special
 import numpy as np
 
-# TODO : Rename the following constants
-
 M = 10.089038980848645
 m = -1.429329123112601
-L = 1
-c = (1 / 2) * (special.psi(L) - np.log(L))
-cn = c / (M - m)  # normalized (0,1) mean of log speckle
