07ed189f3c36d18c32806f1abbcb3745ad86f22f
Hadrien Mariaccia <hadrienmariaccia@gmail.com>
2024-01-18 11:28:07 +0100

    update package version

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/hi-paris/deepdespeckling',
-    version='0.2.4',
+    version='0.2.5',
     zip_safe=False,
 )
